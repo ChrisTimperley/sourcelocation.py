9b98236bc5466504338ced12106ef0679818c457
ChrisTimperley <christimperley@googlemail.com>
2019-12-09 15:37:55 -0500

    added basic setup files

---
+++ setup.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+from glob import glob
+from setuptools import setup, find_packages
+
+MODULE_FILE = \
+    os.path.join(os.path.dirname(__file__), 'src/sourcelocation.py')
+VERSION_REGEX = \
+    r"__version__\s+=\s+['|\"](.*)['|\"]\s*"
+
+with open(MODULE_FILE, 'r') as f:
+    contents = f.read()
+    VERSION = re.search(VERSION_REGEX, contents).group(1)
+
+setup(version=VERSION)
