04496b7756c3c5641495b4c7db3a262cd254ccec
ChrisTimperley <christimperley@googlemail.com>
2019-12-09 15:22:58 -0500

    added placeholder module

---
+++ src/sourcelocation.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+"""
+This module provides a simple set of data structures for representing source
+code locations (e.g., characters, character ranges, lines, etc.).
+"""
+__version__ = '0.0.1'
