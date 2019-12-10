036ad12be504fe777cb67189fcb2a1f7097ebfd0
ChrisTimperley <christimperley@googlemail.com>
2019-12-09 19:13:52 -0500

    added FileLine data structure

--- src/sourcelocation/__init__.py
+++ src/sourcelocation/__init__.py
@@ -5,6 +5,7 @@ code locations (e.g., characters, character ranges, lines, etc.).
 """
 __all__ = (
     '__version__',
+    'FileLine',
     'Location',
     'FileLocation'
 )
@@ -12,6 +13,22 @@ __version__ = '0.0.1'
 
 import typing as _t
 import attr as _attr
+
+
+@_attr.s(frozen=True, slots=True, auto_attribs=True)
+class FileLine:
+    """Represents a one-indexed line within a specific file."""
+    filename: str
+    num: int
+
+    @staticmethod
+    def from_string(s: str) -> 'FileLine':
+        fn, _, s_num = s.rpartition(':')
+        num = int(s_num)
+        return FileLine(fn, num)
+
+    def __str__(self) -> str:
+        return f'{self.filename}:{self.num}'
 
 
 @_attr.s(frozen=True, str=False, auto_attribs=True)
