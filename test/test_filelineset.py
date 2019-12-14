3988a87ac9d5678957494ac41cb39772cb4ff473
ChrisTimperley <christimperley@googlemail.com>
2019-12-15 07:11:16 +0800

    added test_equals

--- test/test_filelineset.py
+++ test/test_filelineset.py
@@ -15,3 +15,14 @@ def contiguous_line_set(start: int, stop: int) -> FileLineSet:
 def test_length():
     set_ = contiguous_line_set(1, 6)
     assert len(set_) == 5
+
+
+def test_equals():
+    set_x = contiguous_line_set(1, 6)
+    set_y = contiguous_line_set(1, 6)
+    set_z = contiguous_line_set(1, 3)
+    assert set_x == set_x
+    assert set_x == set_y
+    assert set_y == set_x
+    assert set_x != set_z
+    assert set_y != set_z
